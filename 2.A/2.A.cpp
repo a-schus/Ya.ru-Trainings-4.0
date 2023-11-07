d647815e87e4121a81e81f6ad1fca44484b6e234
a-schus <a.a.schus@gmail.com>
2023-11-07 12:05:40 +0700

    1

--- 2.A/2.A.cpp
+++ 2.A/2.A.cpp
@@ -13,6 +13,10 @@ std::vector<int> polynomialHash(const std::string& s, std::vector<Ratio> ratios 
 
 std::vector<int> subPolynomialHash(const std::vector<std::vector<int>>& prefixes, int left, int len);
 
+void getPrefixes(const std::string& s, std::vector<std::vector<int>>& resPrefixes, std::vector<Ratio> ratios = ::ratios);
+
+
+
 int main()
 {
 
@@ -27,6 +31,21 @@ std::vector<int> polynomialHash(const std::string& s, std::vector<Ratio> ratios)
 			heshs[k] += (heshs[k] * ratios[k].x + s[i]) % ratios[k].p;
 		}
 	}
-
 	return heshs;
+}
+
+std::vector<int> subPolynomialHash(const std::vector<std::vector<int>>& prefixes, int left, int len)
+{
+
+
+	return std::vector<int>();
+}
+
+void getPrefixes(const std::string& s, std::vector<std::vector<int>>& resPrefixes, std::vector<Ratio> ratios)
+{
+	for (int k = 0; k < ratios.size(); ++k) {
+		for (int i = 0; i < s.length(); ++i) {
+			resPrefixes[k][i] = polynomialHash(s.substr(0, i + 1))[k];
+		}
+	}
 }
