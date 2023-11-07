10c017045b1a17ccfc2f8fe8a41a0f7a410c443c
a-schus <a.a.schus@gmail.com>
2023-11-07 12:35:13 +0700

    1

--- 2.A/2.A.cpp
+++ 2.A/2.A.cpp
@@ -28,7 +28,7 @@ std::vector<int> polynomialHash(const std::string& s, std::vector<Ratio> ratios)
 
 	for (int k = 0; k < ratios.size(); ++k) {
 		for (int i = 0; i < s.length(); ++i) {
-			heshs[k] += (heshs[k] * ratios[k].x + s[i]) % ratios[k].p;
+			heshs[k] = (heshs[k] * ratios[k].x + s[i]) % ratios[k].p;
 		}
 	}
 	return heshs;
@@ -41,11 +41,14 @@ std::vector<int> subPolynomialHash(const std::vector<std::vector<int>>& prefixes
 	return std::vector<int>();
 }
 
-void getPrefixes(const std::string& s, std::vector<std::vector<int>>& resPrefixes, std::vector<Ratio> ratios)
+void getPrefixes(const std::string& s, std::vector<std::vector<Ratio>>& resPrefixes, std::vector<Ratio> ratios)
 {
 	for (int k = 0; k < ratios.size(); ++k) {
-		for (int i = 0; i < s.length(); ++i) {
-			resPrefixes[k][i] = polynomialHash(s.substr(0, i + 1))[k];
+		resPrefixes[k][0].p = s[0] % ratios[k].p;
+		resPrefixes[k][0].x = 1;
+		for (int i = 1; i < s.length(); ++i) {
+			resPrefixes[k][i].p = (resPrefixes[k][i - 1].p * ratios[k].x + s[i]) % ratios[k].p;
+			resPrefixes[k][i].x = (resPrefixes[k][i - 1].x * ratios[k].x) % ratios[k].p;
 		}
 	}
 }
